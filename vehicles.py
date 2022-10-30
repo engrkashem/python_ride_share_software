52f3ec7a18504da2075892ef682a7847457c3338
engrkashem <kashemaust@gmail.com>
2022-10-30 18:34:58 +0600

    abstract class is created in vehicle class and car, bike, cng class created those inherite vehicle class

--- vehicles.py
+++ vehicles.py
@@ -8,15 +8,50 @@ class Vehicle(ABC):
         'cng': 15
     }
 
-    def __init__(self, vehicle_type, rate, driver) -> None:
+    def __init__(self, vehicle_type, license_no, rate, driver) -> None:
         self.vehicle_type = vehicle_type
+        self.license_no = license_no
         self.rate = rate
         self.driver = driver
         self.speed = self.speed[vehicle_type]
 
     @abstractmethod
-    def start(self):
+    def start_driving(self):
         pass
 
-    def move(self):
+    @abstractmethod
+    def trip_finish(self):
         pass
+
+
+class car(Vehicle):
+    def __init__(self, vehicle_type, license_no, rate, driver) -> None:
+        super().__init__(vehicle_type, license_no, rate, driver)
+
+    def start_driving(self):
+        print(self.vehicle_type, self.license_no, 'started')
+
+    def trip_finish(self):
+        print(self.vehicle_type, self.license_no, 'completed trip')
+
+
+class Bike(Vehicle):
+    def __init__(self, vehicle_type, license_no, rate, driver) -> None:
+        super().__init__(vehicle_type, license_no, rate, driver)
+
+    def start_driving(self):
+        print(self.vehicle_type, self.license_no, 'started')
+
+    def trip_finish(self):
+        print(self.vehicle_type, self.license_no, 'completed trip')
+
+
+class Cng(Vehicle):
+    def __init__(self, vehicle_type, license_no, rate, driver) -> None:
+        super().__init__(vehicle_type, license_no, rate, driver)
+
+    def start_driving(self):
+        print(self.vehicle_type, self.license_no, 'started')
+
+    def trip_finish(self):
+        print(self.vehicle_type, self.license_no, 'completed trip')
